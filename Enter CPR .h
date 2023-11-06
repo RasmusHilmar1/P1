04d58ee7572a08c02092b456629473caa5ee39f3
Cecilie <SV26DC@student.aau.dk>
2023-11-06 14:10:10 +0100

    Source + Header: Enter CPR

---
+++ Enter CPR .h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+#ifndef MEDCINE_CPR_NUMBER_H
+#define MEDCINE_CPR_NUMBER_H
+
+#endif //MEDCINE_CPR_NUMBER_H
+
+// A function to type in a CPR number and check if it is in the database
+? CPR_Number (){
+    // Make variables
+
+    printf("Enter CPR number for resident");
+
+// scanf so we can type in a CPR number
+    scanf("%d");
+
+//  It needs to check if the CPR number is in the database
+// Needs an if/else
+
+// If the CPR number is in the database, then the code for patient information is to run
+// Else print "invalid patient" and back to "Enter CPR number"
+
+}
+
+
