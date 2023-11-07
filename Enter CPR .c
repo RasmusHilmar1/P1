293d45c69d7123c546f7f4540c4a0cf7ed3ca666
tobi7602 <tkoch23@student.aau.dk>
2023-11-07 09:21:24 +0100

    start på Enter CPR .c

--- Enter CPR .c
+++ Enter CPR .c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cJSON.h"
 #include "Enter CPR .h"
 
+int EnterCPR() {
+    FILE *fileptr;
 
+    //input and scan for CPR-number
+    printf("Please enter patients CPR-number:\n>");
+    scanf("" CPRnr);
+
+    fileptr = fopen(//json document location,---);
+
+}
